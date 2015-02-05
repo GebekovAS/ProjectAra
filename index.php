c1be0d2e1d7254426f14acd6a98376f415d24ca2
artur <artur@ubuntuServer.(none)>
2015-02-05 12:47:59 +0300

    Переименовал файл.

--- index.php
+++
@@ -1,9 +0,0 @@
-﻿<html>
-  <head>
-    <link href="style.css" rel="stylesheet" type="text/css"/>
-  </head>
-  <body>
-    <div id="servers"></div>
-    <script type="text/javascript" src="script.js"></script>
-  </body>
-</html>
