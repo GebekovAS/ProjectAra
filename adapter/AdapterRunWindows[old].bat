9b53028001dec5d184dfe0743fc65f786b5ea32b
GebekovAS <gebekovas@yandex.ru>
2015-02-06 18:03:32 +0400

    Interface changed:added progressbar state disk

--- adapter/AdapterRunWindows[old].bat
+++
@@ -1,2 +0,0 @@
-cd C:\www\ROOT\monitor\adapter
-for /L %%B in (0 1 150) do (java -classpath mysql-connector-java-5.1.34.jar;json-simple-1.1.1.jar; AdapterEngine 192.168.1.104:3306 dagparus parus2014 && timeout /t 2)
