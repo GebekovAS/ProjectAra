ef1bbb8c212eb903c31e22baffa37dc91d4db070
GebekovAS <GebekovAS@yandex.ru>
2015-02-07 14:40:15 +0300

    Исправил данные

--- adapter/AdapterRunWindows.bat
+++ adapter/AdapterRunWindows.bat
@@ -1,1 +1,1 @@
-java -classpath mysql-connector-java-5.1.34.jar;json-simple-1.1.1.jar; AdapterEngine 192.168.1.104:3306 dagparus parus2014
+java -classpath mysql-connector-java-5.1.34.jar;json-simple-1.1.1.jar; AdapterEngine 192.168.1.3:3306 root password
