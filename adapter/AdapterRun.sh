86a28b956353b7912b05b58bb7f768b60e707261
GebekovAS <GebekovAS@yandex.ru>
2015-02-07 14:04:02 +0300

    Исправление
    Исправил ошибки

--- adapter/AdapterRun.sh
+++ adapter/AdapterRun.sh
@@ -2,6 +2,6 @@
 
 cd /var/www/test/adapter
 for i in $(seq 1 30); do
-  java -classpath mysql-connector-java-5.1.34.jar:json-simple-1.1.1.jar: AdapterEngine 192.168.1.104:3306 dagparus parus2014
+  java -classpath mysql-connector-java-5.1.34.jar:json-simple-1.1.1.jar: AdapterEngine 192.168.1.3:3306 root password
   sleep 2
 done
